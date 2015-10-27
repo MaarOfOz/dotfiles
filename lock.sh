a8e98f0da81c672af1357fd868eb229079d4da8f
sebastian <sebastian@numbat.gojira>
2015-10-27 14:21:34 +0100

    added xwobf tool

--- lock.sh
+++ lock.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-i3lock -c 000000 -i $DIR/lock.png
+SCREEN=/tmp/s.png
+
+xwobf $SCREEN
+convert $SCREEN $DIR/lock.png -composite $SCREEN
+
+i3lock -c 000000 -i $SCREEN
+
+rm $SCREEN
