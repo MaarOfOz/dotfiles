3a8247eb3c27a96acfc788cc655a7dd093ce56af
sebastian <sebastian@numbat.gojira>
2015-10-29 07:47:59 +0100

    removed xwobf

--- lock.sh
+++ lock.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SCREEN=/tmp/s.png
-
-xwobf $SCREEN
-convert $SCREEN $DIR/lock.png -composite $SCREEN
-
-i3lock -c 000000 -i $SCREEN
-
-rm $SCREEN
+i3lock -c 000000
